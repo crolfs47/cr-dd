1e702f04fe3ed2c2bbfffea352e4332eab79343d
Cathy Rolfs <crolfs47@gmail.com>
2024-02-05 12:58:04 -0600

    Add product component, product list helper

---
+++ src/helpers/productList.jsx
@@ -0,0 +1,13 @@
+import image1 from "../assets/1.jpg";
+import image2 from "../assets/2.jpg";
+import image3 from "../assets/3.jpg";
+import image4 from "../assets/4.jpg";
+
+const productList = [
+  { name: "Irish Shamrock Sweatshirt", id: 1, description: "", price: 45, image: image1 },
+  { name: "Irish Felt Appliqué Sweatshirt", id: 2, description: "", price: 45, image: image2 },
+  { name: "Go Irish Sweatshirt", id: 3, description: "", price: 50, image: image3 },
+  { name: "Notre Dame Cheer Hair Bow", id: 4, description: "", price: 20, image: image4 },
+];
+
+export default productList;
